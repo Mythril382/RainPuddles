12dd9150bf8523f11b229167d5c7db8c2e3c05fb
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-07-14 22:31:53 +0800

    gaming

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+Events.run(Trigger.update, () => {
+    Groups.weather.each(w => w.weather instanceof RainWeather, w => {
+        const amount = w.intensity * 20;
+        if(Mathf.chance(w.intensity * 0.95)){
+            const x = Mathf.random(0, Vars.state.map.width);
+            const y = Mathf.random(0, Vars.state.map.height);
+            const tile = Vars.world.tile(x, y);
+            if(tile.block() == Blocks.air) Puddles.deposit(tile, w.weather.liquid, amount);
+        }
+    });
+});
